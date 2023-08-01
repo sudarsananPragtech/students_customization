9b3bb138577792159ed7e694d9cae9bc7fa17a5e
Sudarsanan <sudarsanan.pr@pragtech.co.in>
2023-08-01 11:41:46 +0530

    modified:   students/models/students.py 	modified:   students/views/students.xml

--- students/models/students.py
+++ students/models/students.py
@@ -1,4 +1,8 @@
 from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
+from datetime import date
+
+
 
 class CustomStudent(models.Model):
     _name = 'custom.students'
@@ -6,5 +10,11 @@ class CustomStudent(models.Model):
 
     
     name = fields.Char(string='Name')
-    age = fields.Integer()
+    date_of_birth = fields.Date()
+    age = fields.Integer(compute="compute_age")
     class_name = fields.Char()
+
+    @api.depends('date_of_birth')
+    def compute_age(self):
+        delta = relativedelta(date.today(), self.date_of_birth)
+        self.age = delta.years
